e2f1a14e27ea17340d510b35ee25b1456466a698
kriskenny <115332439+kriskenny@users.noreply.github.com>
2023-04-24 10:27:19 -0700

    Update layout.js

--- src/Components/layout.js
+++ src/Components/layout.js
@@ -1,7 +1,6 @@
-import { createRequire } from 'module';
+
 import React from "react";
 
-const require = createRequire();
 const Layout =({children}) =>{
     return(
         <div className="container">
